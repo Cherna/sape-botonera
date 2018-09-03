11babde170ece069a751e8bc716de7eb21903907
Tomas Chernov <Chernatom@gmail.com>
2018-09-03 01:57:27 -0300

    finish audio gathering script, some minor styling

--- app/index.js
+++ app/index.js
@@ -1,10 +1,27 @@
-/**
- * Application entry point
- */
-
 // Load application styles
 import 'styles/index.scss';
 
-// ================================
-// START YOUR APP HERE
-// ================================
+import React from "react";
+import ReactDOM from "react-dom";
+
+const audioList = require('../audios.json');
+
+const Audio = ({name, src}) =>
+  <audio id="name" controls>
+    <source
+      type="audio/mpeg"
+      src={src}/>
+    Your browser does not support the <code>audio</code> element.
+  </audio>;
+
+const Index = () =>
+  <ul className="audio-list">
+    {Object.keys(audioList).map(audioName =>
+      <li className="audio-container" key={audioName}>
+        <p className="audio-name">{audioName.replace(/_/g, ' ')}</p>
+        <Audio name={audioName} src={audioList[audioName]}/>
+      </li>
+    )}
+  </ul>;
+
+ReactDOM.render( <Index/> , document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
         }),
 
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'index.ejs'),
+            template: path.join(__dirname, 'index.html'),
             title: appHtmlTitle
         })
     ],
